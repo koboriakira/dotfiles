3cf796290aed0467473ae1b3797d1a4c89c26663
Kobori Akira <private.beats@gmail.com>
2021-11-07 14:57:50 +0900

    インストールにgitを必須とする

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -eu
+
+if type "git" > /dev/null 2>&1; then
+  : # do nothing
+else
+  echo "ERROR: 初期設定にはgitが必要です"
+  exit 1
+fi
+
 CURRENT_DIRECTORY=`pwd`
 
 # DOTPATHの設定
@@ -11,17 +19,10 @@ set -u
 DOT_TARBALL="https://github.com/koboriakira/dotfiles/tarball/main"
 REMOTE_URL="git@github.com:koboriakira/dotfiles.git"
 
-# ディレクトリがなければダウンロード（と解凍）する
+# ダウンロード
 echo "Downloading dotfiles..."
 mkdir ${DOTPATH}
-
-if type "git" > /dev/null 2>&1; then
-  git clone --recursive "${REMOTE_URL}" "${DOTPATH}"
-else
-  curl -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
-  tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOTPATH}
-  rm -f ${HOME}/dotfiles.tar.gz
-fi
+git clone --recursive "${REMOTE_URL}" "${DOTPATH}"
 echo $(tput setaf 2)Download dotfiles complete!. ✔︎$(tput sgr0)
 
 # 初期設定、イニシャライズ

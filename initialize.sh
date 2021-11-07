43f7e38cdc38986675a406c51fd50b4f164c8337
Kobori Akira <private.beats@gmail.com>
2021-11-07 15:42:29 +0900

    実行パスを正す

--- initialize.sh
+++ initialize.sh
@@ -10,19 +10,18 @@ if type "brew" > /dev/null 2>&1; then
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   if test -e /etc/os-release ; then
+    # Linuxのときはlinuxbrewのevalを設定
     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $DOTPATH/.zsh/.zsh__temporary.zsh
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   else
-    : # do nothing
+    # Macのときは、brewの基本設定としてman8に書き込み権限を付与
+    chown -R $(whoami) /usr/local/share/man/man1 && chmod u+w /usr/local/share/man/man1
+    echo $(tput setaf 2)Install brew: complete!. ✔︎$(tput sgr0)
   fi
 fi
 
-# brewの基本設定としてman8に書き込み権限を付与
-chown -R $(whoami) /usr/local/share/man/man1 && chmod u+w /usr/local/share/man/man1
-echo $(tput setaf 2)Install brew: complete!. ✔︎$(tput sgr0)
-
 # アプリのインストール
-bash initialize/apps/install.sh
+bash ${DOTPATH}/initialize/apps/install.sh
 echo $(tput setaf 2)Install applications: complete!. ✔︎$(tput sgr0)
 
 # nodebrewのインストール
@@ -30,8 +29,8 @@ curl -L git.io/nodebrew | perl - setup
 echo $(tput setaf 2)Install nodebrew: complete!. ✔︎$(tput sgr0)
 
 # zinitのインストール
-mkdir $HOME/.zinit && cd $_
-git clone https://github.com/zdharma-mirror/zinit.git $HOME/.zinit/bin
+mkdir ${HOME}/.zinit && cd $_
+git clone https://github.com/zdharma-mirror/zinit.git ${HOME}/.zinit/bin
 cd $HOME
 
 # starshipのインストール

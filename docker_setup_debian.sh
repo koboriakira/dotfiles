d48b610ce19a4c44ee290ba3eaf6247e8c02cbf3
Kobori Akira <private.beats@gmail.com>
2021-11-07 23:54:16 +0000

    DebianのDockerイメージに対する初期設定スクリプトを作成

---
+++ docker_setup_debian.sh
@@ -0,0 +1,29 @@
+# 最低限のパッケージを取得
+apt update \
+  && apt upgrade -y \
+  && apt install -y \
+  autoconf \
+  build-essential \
+  curl \
+  git \
+  locales \
+  unzip \
+  wget \
+  sudo \
+  zsh \
+  && apt clean
+
+# rootのパスワードを設定
+echo "root:root" | chpasswd
+
+# ユーザーを作成
+username=doboriakira
+RUN useradd -m "${username}" && \
+  echo "${username}:${username}" | chpasswd && \
+  echo "%${username}    ALL=(ALL)   NOPASSWD:    ALL" > /etc/sudoers.d/${username} && \
+  chmod 0440 /etc/sudoers.d/${username}
+
+# localeの設定
+sed -i -E 's/# (en_US.UTF-8)/\1/' /etc/locale.gen
+sed -i -E 's/# (ja_JP.UTF-8)/\1/' /etc/locale.gen
+locale-gen

a03dbd3520120c502325975b69deaebb1e18bc54
Kobori Akira <private.beats@gmail.com>
2024-04-29 17:45:48 +0900

    wezterm.luaにAdventureTimeカラースキームを追加

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- カラースキームの設定
+config.color_scheme = 'AdventureTime'
+
+return config

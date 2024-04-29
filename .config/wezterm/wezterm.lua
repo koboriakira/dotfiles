923a15db773c5a9fc903115ae36837e73d3b0010
Kobori Akira <private.beats@gmail.com>
2024-04-29 17:58:42 +0900

    weztermの設定を変更しました*** ***背景透過、フォント、フォントサイズ、ショートカットキーを変更しました

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,4 +9,61 @@ end
 -- カラースキームの設定
 config.color_scheme = 'AdventureTime'
 
+-- 背景透過
+config.window_background_opacity = 0.85
+
+-- フォントの設定
+config.font = wezterm.font('PlemolJP Console', {weight='Regular', italic=false})
+
+-- フォントサイズの設定
+config.font_size = 24
+
+-- 最初からフルスクリーンで起動
+-- local mux = wezterm.mux
+-- wezterm.on("gui-startup", function(cmd)
+--     local tab, pane, window = mux.spawn_window(cmd or {})
+--     window:gui_window():toggle_fullscreen()
+-- end)
+
+-- ショートカットキー設定
+local act = wezterm.action
+config.keys = {
+  -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
+  {
+    key = 'f',
+    mods = 'SHIFT|META',
+    action = wezterm.action.ToggleFullScreen,
+  },
+    -- Ctrl+Shift+tで新しいタブを作成
+  {
+    key = 't',
+    mods = 'SHIFT|CTRL',
+    action = act.SpawnTab 'CurrentPaneDomain',
+  },
+  -- Ctrl+Shift+dで新しいペインを作成(画面を分割)
+  {
+    key = 'd',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+    -- Ctrl+左矢印でカーソルを前の単語に移動
+  {
+    key = "LeftArrow",
+    mods = "CTRL",
+    action = act.SendKey {
+      key = "b",
+      mods = "META",
+    },
+  },
+  -- Ctrl+右矢印でカーソルを次の単語に移動
+  {
+    key = "RightArrow",
+    mods = "CTRL",
+    action = act.SendKey {
+      key = "f",
+      mods = "META",
+    },
+  },
+}
+
 return config

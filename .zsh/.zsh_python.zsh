d43501a11840adceec83d4630e548960e95878ff
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-02 17:04:58 +0900

    pipenvのパスを通す

--- .zsh/.zsh_python.zsh
+++ .zsh/.zsh_python.zsh
@@ -3,6 +3,11 @@ if [ -d /opt/homebrew/opt/python3/libexec/bin ]; then
     export PATH=/opt/homebrew/opt/python3/libexec/bin:$PATH
 fi
 
+# ~/.local/bin が存在する場合、PATHに追加
+if [ -d ~/.local/bin ]; then
+    export PATH=~/.local/bin:$PATH
+fi
+
 # プロジェクトの直下に .venv を作成する
 export PIPENV_VENV_IN_PROJECT=1
 export PIPENV_IGNORE_VIRTUALENVS=1

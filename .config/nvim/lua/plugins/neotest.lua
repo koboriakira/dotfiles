b4475f07b84c52f5dc6a576cf9b7c98521eb5c82
Kobori Akira <private.beats@gmail.com>
2024-12-16 17:13:51 +0900

    Add neotest configuration for displaying virtual text, opening output, and handling quickfix

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -14,6 +14,17 @@ return {
       adapters = {
         require("neotest-phpunit")
       },
+      status = { virtual_text = true },
+      output = { open_on_run = true },
+      quickfix = {
+        open = function()
+          if LazyVim.has("trouble.nvim") then
+            require("trouble").open({ mode = "quickfix", focus = false })
+          else
+            vim.cmd("copen")
+          end
+        end,
+      },
     }
   )
   end

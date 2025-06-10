2bed3813d9d3189ff884dadc57065ded3433a631
a-kobori <89172056+a-kobori@users.noreply.github.com>
2025-06-10 12:08:09 +0900

    brewインストールを抽出

--- initialize.sh
+++ initialize.sh
@@ -4,18 +4,7 @@
 touch $DOTPATH/.zsh/.zsh__temporary.zsh
 
 # brewのインストール
-if type "brew" > /dev/null 2>&1; then
-  : # do nothing
-else
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  if test -e /etc/os-release ; then
-    # Linuxのときはlinuxbrewのevalを設定
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $DOTPATH/.zsh/.zsh__temporary.zsh
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  else
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $DOTPATH/.zsh/.zsh__temporary.zsh
-  fi
-fi
+bash ${DOTPATH}/initialize/install_brew.sh
 
 # アプリのインストール
 bash ${DOTPATH}/initialize/apps/install.sh

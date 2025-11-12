63b1f1acd4c0f8014438f80a06d135a434d5bb6a
a-kobori <89172056+a-kobori@users.noreply.github.com>
2025-11-12 18:18:56 +0900

    Remove unused aliases for ripgrep and exa from .zsh_alias.zsh

--- .zsh/.zsh_alias.zsh
+++ .zsh/.zsh_alias.zsh
@@ -23,23 +23,6 @@ alias -g G="| rg"
 alias -g PB="| pbcopy"
 alias -g F="| fzf"
 
-# ripgrep(grep)
-if has "rg"; then
-  alias grep="rg" # Linuxのgrepは'ggrep'で行う
-fi
-
-# exa(ls, tree)
-if has "exa"; then
-  alias ls='exa'                                                         # ls
-  alias l='exa -lbF --git'                                               # list, size, type, git
-  alias ll='exa -lbGF --git'                                             # long list
-  alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
-  alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-  alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-  alias lS='exa -1'                                                      # one column, just names
-  alias lt='exa -lbF --git -T --git-ignore --level=2'                                        # tree
-fi
-
 # bat
 if has "bat"; then
   alias batenv='env | bat'

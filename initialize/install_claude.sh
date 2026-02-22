dbfb4600af7e187d92f9af4e8a39f44e1bfe1184
a-kobori <89172056+a-kobori@users.noreply.github.com>
2026-02-22 17:09:11 +0900

    Remove claude_desktop_config.json and update install script to create a new config file

--- initialize/install_claude.sh
+++ initialize/install_claude.sh
@@ -6,4 +6,5 @@ if [ -e "$HOME/Library/Application Support/Claude/claude_desktop_config.json" ];
 fi
 
 # シンボリックリンクを作成
+touch "$HOME/.dotfiles/claude/claude_desktop_config.json"
 ln -s "$HOME/.dotfiles/claude/claude_desktop_config.json" "$HOME/Library/Application Support/Claude/claude_desktop_config.json"
